4bbca0d02bd9aff72df4e406bf7b145589a06f43
valcubeto <valcubeto@gmail.com>
2026-03-18 01:59:14 -0300

    "just vega run"

--- crates/command-loader/src/lib.rs
+++ crates/command-loader/src/lib.rs
@@ -10,17 +10,21 @@ pub fn run_external(cmd_name: &str, args: &[String]) {
     debug!("Searching command {bin_name:?}.");
     let bin_path = find_external(&bin_name);
     debug!("The command does exist. Executing <{bin_path:?} {}>...", args.join(" "));
+    let command = Command::new(&bin_path)
+        .args(args)
+        .status()
+        .expect("program failed (todo: proper error)");
 }
 
-fn find_external(bin_name: &str) -> Option<PathBuf> {
+fn find_external(bin_name: &str) -> PathBuf {
     let path_var = env::var_os("PATH")
         .expect("failed to read PATH (todo: proper error)");
     for dir in env::split_paths(&path_var) {
         let path = dir.join(bin_name);
         debug!("Trying to load command {path:?}");
         if std::fs::metadata(&path).is_ok()/* _and(|m| m.is_file()) */ {
-            return Some(path)
+            return path;
         }
     }
-    None
+    todo!("command not found");
 }
